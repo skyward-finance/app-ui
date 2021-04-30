aaa66f822a799f9148f1d486ed8f7fbe0a9f961b
Spensa Nightshade <dev@skyward.finance>
2021-04-30 14:14:53 -0700

    Clean up. Add theme

--- src/App.js
+++ src/App.js
@@ -1,51 +1,30 @@
-import React from 'react';
+import React from "react";
 import "error-polyfill";
-import 'bootstrap/dist/js/bootstrap.bundle';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/js/bootstrap.bundle";
 import "./App.scss";
-import './gh-fork-ribbon.css';
-import * as nearAPI from 'near-api-js'
-import Logo from "./images/logo.png"
-import DiscoverPage from "./pages/Discover";
+import * as nearAPI from "near-api-js";
+import Logo from "./images/logo_horizontal_white.png";
 import HomePage from "./pages/Home";
-import {BrowserRouter as Router, Link, Route, Switch} from 'react-router-dom'
-import {fromNear} from "./components/BuyButton";
+import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
 import ls from "local-storage";
-import CardPage from "./pages/Card";
-import AccountPage from "./pages/Account";
-import StatsPage from "./pages/Stats";
 
 const IsMainnet = window.location.hostname === "berry.cards";
 const TestNearConfig = {
-  networkId: 'testnet',
-  nodeUrl: 'https://rpc.testnet.near.org',
-  archivalNodeUrl: 'https://rpc.testnet.internal.near.org',
-  contractName: 'dev-1614796345972-8721304',
-  walletUrl: 'https://wallet.testnet.near.org',
+  networkId: "testnet",
+  nodeUrl: "https://rpc.testnet.near.org",
+  archivalNodeUrl: "https://rpc.testnet.internal.near.org",
+  contractName: "dev-1614796345972-8721304",
+  walletUrl: "https://wallet.testnet.near.org",
 };
 const MainNearConfig = {
-  networkId: 'mainnet',
-  nodeUrl: 'https://rpc.mainnet.near.org',
-  archivalNodeUrl: 'https://rpc.mainnet.internal.near.org',
-  contractName: 'cards.berryclub.ek.near',
-  walletUrl: 'https://wallet.near.org',
+  networkId: "mainnet",
+  nodeUrl: "https://rpc.mainnet.near.org",
+  archivalNodeUrl: "https://rpc.mainnet.internal.near.org",
+  contractName: "cards.berryclub.ek.near",
+  walletUrl: "https://wallet.near.org",
 };
 
 const NearConfig = IsMainnet ? MainNearConfig : TestNearConfig;
-
-const FetchLimit = 50;
-
-const mapAccount = (a) => {
-  return {
-    requests: a.requests,
-    numCards: a.num_cards,
-    purchaseVolume: fromNear(a.purchase_volume),
-    numPurchases: a.num_purchases,
-    saleProfit: fromNear(a.sale_profit),
-    numSales: a.num_sales,
-    numVotes: a.num_votes,
-  };
-}
 
 class App extends React.Component {
   constructor(props) {
@@ -53,15 +32,13 @@ class App extends React.Component {
 
     this._near = {};
 
-    this._near.lsKey = NearConfig.contractName + ':v01:';
-    this._near.lsKeyRecentCards = this._near.lsKey + "recentCards";
+    this._near.lsKey = NearConfig.contractName + ":v01:";
 
     this.state = {
       connected: false,
       isNavCollapsed: true,
       account: null,
       requests: null,
-      recentCards: ls.get(this._near.lsKeyRecentCards) || [],
     };
 
     this._initNear().then(() => {
@@ -73,74 +50,67 @@ class App extends React.Component {
     });
   }
 
-
   async _initNear() {
     const keyStore = new nearAPI.keyStores.BrowserLocalStorageKeyStore();
-    const near = await nearAPI.connect(Object.assign({deps: {keyStore}}, NearConfig));
+    const near = await nearAPI.connect(
+      Object.assign({ deps: { keyStore } }, NearConfig)
+    );
     this._near.keyStore = keyStore;
     this._near.near = near;
 
-    this._near.walletConnection = new nearAPI.WalletConnection(near, NearConfig.contractName);
+    this._near.walletConnection = new nearAPI.WalletConnection(
+      near,
+      NearConfig.contractName
+    );
     this._near.accountId = this._near.walletConnection.getAccountId();
 
     this._near.account = this._near.walletConnection.account();
-    this._near.contract = new nearAPI.Contract(this._near.account, NearConfig.contractName, {
-      viewMethods: ['get_account', 'get_num_accounts', 'get_accounts', 'get_num_cards', 'get_top', 'get_rating', 'get_trade_data', 'get_card_info', 'get_account_cards'],
-      changeMethods: ['register_account', 'vote', 'buy_card'],
-    });
-
-    this._near.accounts = {};
-
-    this._near.getAccount = (accountId) => {
-      if (accountId in this._near.accounts) {
-        return this._near.accounts[accountId];
+    this._near.contract = new nearAPI.Contract(
+      this._near.account,
+      NearConfig.contractName,
+      {
+        viewMethods: [
+          "balance_of",
+          "balances_of",
+          "get_num_balances",
+          "get_subscribed_sales",
+          "get_account_sales",
+          "get_sale",
+          "get_sales",
+          "get_treasury_balance",
+          "get_treasury_balances",
+          "get_treasury_num_balances",
+          "get_skyward_token_id",
+          "get_skyward_total_supply",
+          "get_listing_fee",
+        ],
+        changeMethods: [
+          "register_token",
+          "register_tokens",
+          "withdraw_token",
+          "donate_token_to_treasury",
+          "sale_create",
+          "sale_deposit_out_token",
+          "sale_deposit_in_token",
+          "sale_withdraw_in_token",
+          "sale_distribute_unclaimed_tokens",
+          "sale_claim_out_tokens",
+          "redeem_skyward",
+        ],
       }
-      return this._near.accounts[accountId] = Promise.resolve((async () => {
-        const a = await this._near.contract.get_account({account_id: accountId});
-        const account = a ? mapAccount(a) : null;
-        if (account) {
-          account.fetchCards = () => {
-            if (account.cardFetching) {
-              return account.cardFetching;
-            }
-            const promises = [];
-            for (let i = 0; i < account.numCards; i += FetchLimit) {
-              promises.push(this._near.contract.get_account_cards({
-                account_id: accountId,
-                from_index: i,
-                limit: FetchLimit,
-              }));
-            }
-            return account.cardFetching = Promise.resolve((async () => {
-              return (await Promise.all(promises)).flat();
-            })());
-          };
-        }
-        return account;
-      })());
-    };
+    );
 
     if (this._near.accountId) {
-      let account = await this._near.getAccount(this._near.accountId);
-      if (account === null) {
-        await this._near.contract.register_account();
-        delete this._near.accounts[this._near.accountId];
-        account = await this._near.getAccount(this._near.accountId);
-      }
-      this.setState({
-        account,
-        requests: account.requests,
-      });
     }
   }
 
   async requestSignIn(e) {
     e && e.preventDefault();
-    const appTitle = 'Berry Cards';
+    const appTitle = "Skyward Finance";
     await this._near.walletConnection.requestSignIn(
       NearConfig.contractName,
       appTitle
-    )
+    );
     return false;
   }
 
@@ -148,42 +118,15 @@ class App extends React.Component {
     this._near.walletConnection.signOut();
     this._near.accountId = null;
     this.setState({
-      signedIn: !!this._accountId,
-      signedAccountId: this._accountId,
-    })
-  }
-
-  popRequest(c) {
-    const requests = this.state.requests.slice(1);
-    this.setState({
-      requests,
-    }, c);
-  }
-
-  addRequest(r, c) {
-    const requests = this.state.requests.slice(0);
-    requests.push(r);
-    this.setState({
-      requests,
-    }, c);
-  }
-
-  addRecentCard(cardId) {
-    let recentCards = this.state.recentCards.slice(0);
-    const index = recentCards.indexOf(cardId);
-    if (index !== -1) {
-      recentCards.splice(index, 1);
-    }
-    recentCards.unshift(cardId);
-    recentCards = recentCards.slice(0, 5);
-    ls.set(this._near.lsKeyRecentCards, recentCards);
-    this.setState({
-      recentCards
-    })
+      signedIn: false,
+      signedAccountId: null,
+    });
   }
 
   async refreshAllowance() {
-    alert("You're out of access key allowance. Need sign in again to refresh it");
+    alert(
+      "You're out of access key allowance. Need sign in again to refresh it"
+    );
     await this.logOut();
     await this.requestSignIn();
   }
@@ -192,96 +135,98 @@ class App extends React.Component {
     const passProps = {
       _near: this._near,
       updateState: (s, c) => this.setState(s, c),
-      popRequest: (c) => this.popRequest(c),
-      addRequest: (r, c) => this.addRequest(r, c),
-      addRecentCard: (cardId) => this.addRecentCard(cardId),
       refreshAllowance: () => this.refreshAllowance(),
-      ...this.state
+      ...this.state,
     };
     const header = !this.state.connected ? (
-      <div>Connecting... <span className="spinner-grow spinner-grow-sm" role="status" aria-hidden="true"></span></div>
-    ) : (this.state.signedIn ? (
       <div>
-        <button
-          className="btn btn-outline-secondary"
-          onClick={() => this.logOut()}>Sign out ({this.state.signedAccountId})</button>
+        Connecting...{" "}
+        <span
+          className="spinner-grow spinner-grow-sm"
+          role="status"
+          aria-hidden="true"
+        />
+      </div>
+    ) : this.state.signedIn ? (
+      <div>
+        <button className="btn btn-outline-light" onClick={() => this.logOut()}>
+          Sign out ({this.state.signedAccountId})
+        </button>
       </div>
     ) : (
       <div>
         <button
-          className="btn btn-primary"
-          onClick={(e) => this.requestSignIn(e)}>Sign in with NEAR Wallet</button>
+          className="btn btn-outline-light"
+          onClick={(e) => this.requestSignIn(e)}
+        >
+          Sign in with NEAR Wallet
+        </button>
       </div>
-    ));
+    );
 
     return (
       <div className="App">
         <Router basename={process.env.PUBLIC_URL}>
-          <nav className="navbar navbar-expand-lg navbar-light bg-light mb-3">
+          <nav className="navbar navbar-expand-lg navbar-dark bg-primary mb-3">
             <div className="container-fluid">
-              <Link className="navbar-brand" to="/" title="NEAR.fm">
-                <img src={Logo} alt="Berry Cards" className="d-inline-block align-middle" />
-                [BETA] Berry Cards
+              <Link className="navbar-brand" to="/" title="Skyward Finance">
+                <img
+                  src={Logo}
+                  alt="Skyward Finance"
+                  className="d-inline-block align-middle"
+                />
               </Link>
-              <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
-                      data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                      aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
+              <button
+                className="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+              >
+                <span className="navbar-toggler-icon" />
               </button>
-              <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <div
+                className="collapse navbar-collapse"
+                id="navbarSupportedContent"
+              >
                 <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                   <li className="nav-item">
-                    <Link className="nav-link" aria-current="page" to="/">Home</Link>
+                    <Link className="nav-link" aria-current="page" to="/">
+                      Home
+                    </Link>
                   </li>
                   {this.state.signedIn && (
                     <li className="nav-item">
-                      <Link className="nav-link" aria-current="page" to="/discover">Discover</Link>
-                    </li>
-                  )}
-                  {this.state.signedIn && (
-                    <li className="nav-item">
-                      <Link className="nav-link" aria-current="page"
-                            to={`/a/${this.state.signedAccountId}`}>Profile</Link>
+                      <Link
+                        className="nav-link"
+                        aria-current="page"
+                        to={`/a/${this.state.signedAccountId}`}
+                      >
+                        Profile
+                      </Link>
                     </li>
                   )}
                   <li className="nav-item">
-                    <Link className="nav-link" aria-current="page" to="/stats">Stats</Link>
+                    <Link className="nav-link" aria-current="page" to="/stats">
+                      Stats
+                    </Link>
                   </li>
                 </ul>
-                <form className="d-flex">
-                  {header}
-                </form>
+                <form className="d-flex">{header}</form>
               </div>
             </div>
           </nav>
 
-          <a className="github-fork-ribbon right-bottom fixed" href="https://github.com/evgenykuzyakov/berry-hot" data-ribbon="Fork me on GitHub"
-             title="Fork me on GitHub">Fork me on GitHub</a>
-
           <Switch>
             <Route exact path={"/"}>
-              {this.state.signedIn ? (
-                <HomePage {...passProps}/>
-              ) : (
-                <DiscoverPage {...passProps}/>
-              )}
-            </Route>
-            <Route exact path={"/discover"}>
-              <DiscoverPage {...passProps}/>
-            </Route>
-            <Route exact path={"/stats"}>
-              <StatsPage {...passProps}/>
-            </Route>
-            <Route exact path={"/a/:accountId"}>
-              <AccountPage {...passProps} />
-            </Route>
-            <Route exact path={"/c/:cardId"}>
-              <CardPage {...passProps} />
+              <HomePage {...passProps} />
             </Route>
           </Switch>
         </Router>
       </div>
-    )
+    );
   }
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,121 +1,17 @@
 import "./Home.scss";
-import React, {useState} from 'react';
-import { CardImage, preloadCard } from "../components/CardImage";
-import CardPreview from "../components/CardPreview";
+import React, { useState } from "react";
 import uuid from "react-uuid";
 
-const SelectedLeft = "SelectedLeft";
-const SelectedRight = "SelectedRight";
-const Skipped = "Skipped";
+function HomePage(props) {
+  const [gkey] = useState(uuid());
 
-const DefaultTGas = 100;
-const ExtraTGas = 40;
-const NumIter = 5;
-
-  function HomePage(props) {
-  const [leftReady, setLeftReady] = useState(false);
-  const [rightReady, setRightReady] = useState(false);
-  const [votingPromise] = useState(Promise.resolve());
-  const [gkey] = useState(uuid())
-
-  const voteRequest = props.requests ? props.requests[0] : null;
-
-  if (props.requests) {
-    props.requests.slice(1).forEach((nextRequest) => {
-      preloadCard(nextRequest.left);
-      preloadCard(nextRequest.right);
-    });
-  }
-
-  const vote = async (e, voteRequest, response) => {
-    e.preventDefault();
-    if (response === SelectedLeft && !leftReady) {
-      return;
-    }
-    if (response === SelectedRight && !rightReady) {
-      return;
-    }
-    props.popRequest();
-    votingPromise.then(async () => {
-      let tgas = DefaultTGas;
-      let newRequest = null
-      for (let iter = 0; iter < NumIter; ++iter) {
-        try {
-          newRequest = await props._near.contract.vote({
-            request: voteRequest,
-            response,
-          }, tgas.toString() + "000000000000");
-        } catch (e) {
-          const msg = e.toString();
-          console.warn(msg);
-          if (msg.indexOf("prepaid gas") !== -1) {
-            tgas += ExtraTGas;
-            continue
-          }
-          if (msg.indexOf("does not have enough balance") !== -1) {
-            await props.refreshAllowance();
-            return;
-          }
-        }
-        break;
-      }
-      if (newRequest) {
-        if (response === SelectedLeft || response === SelectedRight) {
-          const cardId = response === SelectedLeft ? voteRequest.left : voteRequest.right;
-          props.addRecentCard(cardId);
-        }
-        props.addRequest(newRequest);
-      }
-    });
-  }
-
-  const cards = props.recentCards.map((cardId) => {
-    const key = `${gkey}-${cardId}`;
-    return (
-      <CardPreview {...props} key={key} cardId={cardId} />
-    );
-  })
+  const cards = "";
 
   return (
     <div>
       <div className="container">
-        {voteRequest ? (
-          <div>
-            <h3>Vote</h3>
-            <div className="row justify-content-md-center mb-3">
-              <div className="col col-sm-6">
-                <div
-                  className={`card-picker${!leftReady ? " disabled": ""}`}
-                  onClick={(e) => vote(e, voteRequest, SelectedLeft)}
-                >
-                  <CardImage className="img-fluid" cardId={voteRequest.left} cardReady={setLeftReady}/>
-                </div>
-              </div>
-              <div className="col col-sm-6">
-                <div
-                  className={`card-picker${!rightReady ? " disabled": ""}`}
-                  onClick={(e) => vote(e, voteRequest, SelectedRight)}
-                >
-                  <CardImage className="img-fluid" cardId={voteRequest.right} cardReady={setRightReady}/>
-                </div>
-              </div>
-            </div>
-            <div className="row justify-content-md-center mb-3">
-              <button
-                className="btn btn-lg btn-danger"
-                onClick={(e) => vote(e, voteRequest, Skipped)}
-              >Skip both cards</button>
-            </div>
-          </div>
-        ) : (
-          <div className="d-flex justify-content-center">
-            <div className="spinner-grow" role="status">
-              <span className="visually-hidden">Loading...</span>
-            </div>
-          </div>
-        )}
         <div className="row justify-content-md-center mb-3">
-          {(cards.length > 0) && (
+          {cards.length > 0 && (
             <div>
               <h3>Recent votes</h3>
               {cards}
